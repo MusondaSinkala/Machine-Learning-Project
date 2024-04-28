a5495a0d4e9f203f7b3f9c145ea43a8d022b9283
Musonda Sinkala <68292754+MusondaSinkala@users.noreply.github.com>
2024-04-28 07:47:45 -0400

    Update Random Forest Model - grid search applied

--- Python code/Random Forest Model.py
+++ Python code/Random Forest Model.py
@@ -1,46 +1,39 @@
 # load the packages
-import random
 import os
+import pickle
+import inspect
 import pandas as pd
-from sklearn.model_selection import train_test_split, cross_val_score, GroupKFold
+import numpy as np
+from sklearn.model_selection import cross_val_score, GroupKFold, RandomizedSearchCV
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 import seaborn as sns
 import matplotlib.pyplot as plt
 
 # load the data
-cwd = os.getcwd()
-data = pd.read_csv(cwd + f"\\Data\\histopathological_data.csv")
-data = data[data.columns[2:].values] # Exclude the directory path and files columns
+cwd        = os.getcwd()
+data       = pd.read_csv(cwd + f"\\Data\\histopathological_data.csv")
+train_data = pd.read_csv(cwd + f"\\Data\\training_histopathological_data.csv")
+test_data  = pd.read_csv(cwd + f"\\Data\\testing_histopathological_data.csv")
+data       = data[data.columns[2:].values] # Exclude the directory path and files columns
+train_data = train_data[train_data.columns[1:].values] # Exclude the directory path and files columns
+test_data  = test_data[test_data.columns[1:].values] # Exclude the directory path and files columns
 data.head()
-
-# Random, small sample from the data
-sample_data = data#.sample(n = 300000, random_state = 7)
-
-# Extract unique PatientID values
-unique_patient_ids = sample_data['PatientID'].unique()
-
-# Randomly shuffle the unique PatientID values
-random.shuffle(unique_patient_ids)
-
-# Split the shuffled PatientID values into training and test sets (e.g., 80% train, 20% test)
-train_patient_ids, test_patient_ids = train_test_split(unique_patient_ids, test_size = 0.2, random_state = 7)
-
-# Filter the original dataset to create the training and test sets
-train_data = data[data['PatientID'].isin(train_patient_ids)]
-test_data = data[data['PatientID'].isin(test_patient_ids)]
+train_data.head()
+test_data.head()
 
 X_train = train_data.drop(['PatientID', 'cancer'], axis = 1) # Dropping both PatientID and the target variable (cancer)
 y_train = train_data['cancer']
-X_test = test_data.drop(['PatientID', 'cancer'], axis = 1) # Dropping both PatientID and the target variable (cancer)
-y_test = test_data['cancer']
+X_test  = test_data.drop(['PatientID', 'cancer'], axis = 1) # Dropping both PatientID and the target variable (cancer)
+y_test  = test_data['cancer']
 
-# Exclude the PatientID feature
-X = sample_data.drop(['cancer'], axis = 1)  # Dropping both PatientID and the target variable (cancer)
-y = sample_data['cancer']
+X = data.drop(['cancer'], axis = 1)  # Dropping both PatientID and the target variable (cancer)
+y = data['cancer']
 
 # Build a Random Forest model
-rf_model = RandomForestClassifier(n_estimators = 100, random_state = 7)
+rf_model = RandomForestClassifier(n_estimators = 200, random_state = 7, min_samples_split = 4,
+                                  min_samples_leaf = 1, max_depth = 30, criterion = 'entropy',
+                                  bootstrap = False)
 rf_model.fit(X_train, y_train)
 
 # Make predictions
@@ -105,3 +98,67 @@ plt.xlabel("Feature")
 plt.ylabel("Importance")
 plt.tight_layout()
 plt.show()
+
+### Save to Pickle File
+def is_serializable(obj):
+    """
+    Check if an object is serializable.
+    """
+    try:
+        pickle.dumps(obj)
+        return True
+    except:
+        return False
+
+def save_locals_to_pickle(filename):
+    # Get local variables
+    caller_locals = inspect.currentframe().f_back.f_locals.copy()
+
+    # Filter out non-serializable objects
+    serializable_locals = {k: v for k, v in caller_locals.items() if is_serializable(v)}
+
+    # Store local variables in a pickle file
+    try:
+        with open(filename, 'wb') as f:
+            pickle.dump(serializable_locals, f)
+            print("Local variables saved to", filename)
+    except Exception as e:
+        print("Error occurred while saving:", e)
+
+# Example usage
+filename = 'RFModel.pkl'
+save_locals_to_pickle(filename)
+print('')
+
+#######################################################################################################################
+
+# Define the model
+rf = RandomForestClassifier(random_state = 7)
+
+# Define the parameter grid
+param_dist = {
+    'n_estimators': np.arange(50, 300, 50),
+    'max_depth': [None, 10, 20, 30],
+    'min_samples_split': [2, 4, 8],
+    'min_samples_leaf': [1, 2, 4],
+    'bootstrap': [True, False],
+    'criterion': ['gini', 'entropy']
+}
+
+# Create the RandomizedSearchCV
+random_search = RandomizedSearchCV(
+    estimator = rf,
+    param_distributions = param_dist,
+    n_iter = 50,  # Number of random iterations
+    scoring = 'accuracy',
+    cv = 3,  # Number of cross-validation folds
+    random_state = 7,
+    n_jobs = -1  # Use all processors
+)
+
+# Fit RandomizedSearchCV
+random_search.fit(X_train, y_train)
+
+# Print best parameters and best score
+print("Best parameters:", random_search.best_params_)
+print("Best cross-validation score:", random_search.best_score())
